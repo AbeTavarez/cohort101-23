8bb3b1a178cd4e3b7e89e8a5f5cc05b27c11a181
Abraham E. Tavarez <abeincwireless@gmail.com>
2023-06-22 16:55:25 -0400

    variables in js

---
+++ Mod1/JS-variables/script.js
@@ -0,0 +1,37 @@
+// we can print stuff to the console
+console.log("script is running!");
+
+/**
+ * this
+ * is 
+ * a
+ * multi-line
+ * comment
+ */
+
+// ====== Variables ================
+
+var myVariables = "some value"; // string
+
+let num1 = 11; // number (int)
+
+const PI = 3.14; //number (decimal)
+
+let isAdult = true; // boolean
+
+let roomNumber = '205'; // string
+
+let someVar = `hello world!`; // string
+
+let carMilage = 35657; // number
+
+let _favFood = 'pizza'; // string
+
+var var2; // undefined
+let var3;
+const var4 = 77;
+
+console.log(myVariables);
+console.log(num1);
+console.log(isAdult);
+console.log(var2);
