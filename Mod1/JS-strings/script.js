be535d9da285a6b683bb80faa15a1f9089dbff23
Abraham E. Tavarez <abeincwireless@gmail.com>
2023-06-23 16:42:24 -0400

    js strings p-1

---
+++ Mod1/JS-strings/script.js
@@ -0,0 +1,10 @@
+console.log('running');
+
+
+console.log('Hello' + ' World!');
+console.log('This is the number ' + 5); // type coercion
+console.log("Hello" - "JS"); // NaN -> Not A Number
+
+const userName = "Inigo Montoya";
+userName[6] = "X";
+console.log(userName[6]);
