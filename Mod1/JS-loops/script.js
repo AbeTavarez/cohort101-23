dcb639de2316b243f3d96d71a9cece14023db3ca
Abraham E. Tavarez <abeincwireless@gmail.com>
2023-06-27 21:05:46 -0400

    loops exercises

--- Mod1/JS-loops/script.js
+++ Mod1/JS-loops/script.js
@@ -1,7 +1,13 @@
+//* === Structure of a forLoop =========== //
 
-// === Structure of a forLoop =========== //
+//* (initialization; condition; incrementor)
+for (let counter = 0; counter <= 10; counter++) {
+    // console.log(counter);
+}
 
-// (initialization; condition; incrementor)
+//* Exercise
+//* Write a for loop that counts down from 10 to 1.
+
 for (let counter = 10; counter >= 1; counter--) {
     // console.log(counter);
 }
@@ -11,7 +17,8 @@ for (let i = 2; i <= 10; i+= 2) {
     // console.log(`Counter is ${i}`);
 }
 
-// Write a for loop for the given output: 1, 3, 5, 7, 9 2, 4, 6, 8, 10
+//* Exercise
+//* Write a for loop for the given output: 1, 3, 5, 7, 9 2, 4, 6, 8, 10
 for (let x = 1; x <= 9; x ++) {
     // console.log(x);
 
@@ -26,12 +33,38 @@ for (let x = 1; x <= 9; x ++) {
     }
 }
 
+//* Exercise
+//* create a loop that outputs multiples of 3 starting at 6 ending at 60.
+for (let j = 6; j <= 60; j+=3) {
+    // console.log(j);
+}
 
+// Exercise
+// Write a for loop for the given output: ######
+let hash = '#';
+for(let z = 0; z <= 6; z++) {
+    // console.log(hash);
+    hash+= '#';
+}
+
+//* Print even numbers
+//* Using the remainder % operator, write a for loop that iterates between 0 and 10 and prints all even numbers.
+for(let r = 0; r <= 10; r++) {
+    if (r % 2 === 0){
+        console.log(`${r} is an even number.`);
+    } else {
+        console.log(`${r} is an odd number.`);
+    }
+}
+
+
+
+
+//* ===== Looping over a string ===== 
 console.log("Hello World!".length);
 
 let str = "Hello World!";
 // console.log( str[1] );
-
 
 
 for (let index = 0; index <= str.length; index++) {
@@ -40,10 +73,11 @@ for (let index = 0; index <= str.length; index++) {
 
 
 
-// =========== While Loops =============== //
+//* =========== While Loops =============== //
+//* Structure of a while loop
+// while(condition){}
+
 let a = 1;
-
-
 while(a <= 10) {
 
     if (a % 2 === 0 ) {
