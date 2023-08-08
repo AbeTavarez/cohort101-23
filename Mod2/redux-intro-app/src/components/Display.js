5aab7bc68322216a804257604ee09b586f4412d1
Abraham E. Tavarez <abeincwireless@gmail.com>
2023-08-08 17:03:11 -0400

    Redux ToolKit Counter and Todos App

--- Mod2/redux-intro-app/src/components/Display.js
+++ Mod2/redux-intro-app/src/components/Display.js
@@ -4,7 +4,7 @@ export default function Display() {
 
     useEffect(() => {
         const getData = async () => {
-            const res = await fetch('https://financialmodelingprep.com/api/v3/quote-short/AAPL?apikey=9af78c8bd5456131afa1089ff015860f');
+            const res = await fetch('https://financialmodelingprep.com/api/v3/quote-short/AAPL?apikey=');
             const data = await res.json();
             console.log(data);
         }
